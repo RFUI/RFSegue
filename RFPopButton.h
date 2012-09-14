56eb7e3a6a13bd2a86f6bc6bfcc1a1d12d8e1d21
XiaGuanghua <xiaguanghua@chinamobo.com>
2012-09-14 17:18:19 +0800

    Add UIButton subclass for pop and dismiss modal

---
+++ RFPopButton.h
@@ -0,0 +1,14 @@
+//
+//  RFPopButton.h
+//  CrazyPurse
+//
+//  Created by 夏光华 on 12-9-14.
+//  Copyright (c) 2012年 mobochina. All rights reserved.
+//
+
+#import "RFUI.h"
+
+@interface RFPopButton : UIButton
+@property (RF_WEAK, nonatomic) IBOutlet UIViewController *masterViewController;
+
+@end
